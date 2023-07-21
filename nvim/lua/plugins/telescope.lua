552e837d57710049ae41671bc714dde71e68e32c
strbagus <satrio.baguswbw@gmail.com>
2023-07-22 01:26:41 +0700

    Nvim: Telescope ignore folder
    fixed typo `file_ignore_pattern`

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   opts = {
     defaults = {
-      file_ignore_pattern = { "node_modules", "vendor" }
+      file_ignore_patterns = { "node_modules", "vendor" }
     },
   },
   config = function(_, opts)

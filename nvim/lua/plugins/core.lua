a9117d69d1acfd505337e24b701f659b38b8ee80
strbagus <satrio.baguswbw@gmail.com>
2023-10-02 23:04:27 +0700

    Nvim: upgrade indent-blankline v3!

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -59,12 +59,9 @@ return {
   },
   {
     'lukas-reineke/indent-blankline.nvim',
-    opts = {
-      show_current_context = true,
-      show_current_context_start = true,
-    },
-    config = function(_, opts)
-      require('indent_blankline').setup(opts)
+    main = 'ibl',
+    config = function()
+      require('ibl').setup()
     end
   }
 }

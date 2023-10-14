ec26555d18262164966d892fae4ed66e51dc4ba8
strbagus <satrio.baguswbw@gmail.com>
2023-10-15 03:29:57 +0700

    Nvim: lsp-zero keybind remap into default

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -17,10 +17,6 @@ return {
     })
     lsp.on_attach(function(_, bufnr)
       lsp.default_keymaps({buffer = bufnr})
-      local opts = { buffer = bufnr, remap = false }
-      vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-      vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-      vim.keymap.set('n', 'gf', function() vim.lsp.buf.references() end, opts)
     end)
     lsp.setup()
 

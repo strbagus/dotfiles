10ccff778b05d55fb563e14883be677199ad2f47
Satrio Bagus <satrio.baguswbw@gmail.com>
2023-12-01 18:26:44 +0700

    Nvim: add treesitter laravel-blade support

---
+++ nvim/after/queries/blade/highlights.scm
@@ -0,0 +1,5 @@
+(directive) @tag
+(directive_start) @tag
+(directive_end) @tag
+(bracket) @tag.delimiter
+(comment) @comment @spell
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,18 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  dependencies = { "windwp/nvim-ts-autotag", "JoosepAlviste/nvim-ts-context-commentstring" },
-	build = ":TSUpdate",
-	opts = {
-		ensure_installed = {
-			"lua", "vim"
-		},
+  --[[ init = function()
+    require("core.utils").lazy_load "nvim-treesitter"
+  end, ]]
+  dependencies = { 
+    "windwp/nvim-ts-autotag", 
+    -- "JoosepAlviste/nvim-ts-context-commentstring" 
+  },
+  cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+  build = ":TSUpdate",
+  opts = {
+    ensure_installed = {
+      "lua", "vim"
+    },
     highlight = {
       enable = true,
       disable = function(_, bufnr) return vim.api.nvim_buf_line_count(bufnr) > 10000 end,
@@ -13,8 +20,27 @@ return {
     incremental_selection = { enable = true },
     indent = { enable = true },
     autotag = { enable = true },
-    context_commentstring = { enable = true, enable_autocmd = false },
-	},
+    -- context_commentstring = { enable = true, enable_autocmd = false },
+  },
 
-	config = function(_,opts) require("nvim-treesitter.configs").setup(opts) end
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+
+    local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+
+    parser_config.blade = {
+      install_info = {
+        url = "https://github.com/EmranMR/tree-sitter-blade",
+        files = { "src/parser.c" },
+        branch = "main",
+      },
+      filetype = "blade"
+    }
+
+    vim.filetype.add({
+      pattern = {
+        ['.*%.blade%.php'] = 'blade',
+      },
+    })
+  end
 }

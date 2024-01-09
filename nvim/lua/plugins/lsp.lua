a1863a6ea4fd802215b90fc0834817f80fa202cb
Satrio Bagus <strbagus@deblp.local>
2024-01-09 04:39:09 -0800

    Nvim: turn on lsp-zero default keybind

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -20,6 +20,9 @@ return {
         lsp.default_setup
       }
     })
+    lsp.on_attach(function(client, bufnr)
+      lsp.default_keymaps({buffer = bufnr})
+    end)
     require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
   end
 }

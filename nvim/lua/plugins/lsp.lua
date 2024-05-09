85665eeecde4b5079dd512f4a2b62d6bb920c19c
Satrio Bagus <satrio.baguswbw@gmail.com>
2024-05-08 18:23:27 -0700

    nvim: rewrite lsp configuration

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,28 +1,45 @@
 return {
   {
-    "williamboman/mason.nvim",
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+      'neovim/nvim-lspconfig'
+    },
     config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver" }
+      local lsp_zero = require('lsp-zero')
+      lsp_zero.on_attach(function(_, bufnr)
+        lsp_zero.default_keymaps({ buffer = bufnr })
+      end)
+
+      require('mason').setup({})
+      require('mason-lspconfig').setup({
+        ensure_installed = {
+          'lua_ls',
+          'tsserver',
+          'volar'
+        },
+        handlers = {
+          function(server_name)
+            local initConfig = {}
+            if server_name == 'volar' then
+              initConfig = {
+                filetypes = { "vue", },
+                init_options = {
+                  vue = {
+                    hybridMode = false,
+                  },
+                  typescript = {
+                    tsdk = '/usr/lib/node_modules/@vue/typescript-plugin'
+                  }
+                }
+              }
+            end
+            require('lspconfig')[server_name].setup(initConfig)
+          end,
+        },
       })
     end
   },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
-
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  }
 }

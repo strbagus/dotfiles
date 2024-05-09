9266b4d66db27b7ac7afa59bf6a5247e98a6eb95
Satrio Bagus <satrio.baguswbw@gmail.com>
2024-05-08 20:07:06 -0700

    nvim: lsp formatting & add ensure installed

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,45 +1,45 @@
 return {
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
-    dependencies = {
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
-      'neovim/nvim-lspconfig'
-    },
-    config = function()
-      local lsp_zero = require('lsp-zero')
-      lsp_zero.on_attach(function(_, bufnr)
-        lsp_zero.default_keymaps({ buffer = bufnr })
-      end)
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"neovim/nvim-lspconfig",
+		},
+		config = function()
+			local lsp_zero = require("lsp-zero")
+			lsp_zero.on_attach(function(_, bufnr)
+				lsp_zero.default_keymaps({ buffer = bufnr })
+			end)
 
-      require('mason').setup({})
-      require('mason-lspconfig').setup({
-        ensure_installed = {
-          'lua_ls',
-          'tsserver',
-          'volar'
-        },
-        handlers = {
-          function(server_name)
-            local initConfig = {}
-            if server_name == 'volar' then
-              initConfig = {
-                filetypes = { "vue", },
-                init_options = {
-                  vue = {
-                    hybridMode = false,
-                  },
-                  typescript = {
-                    tsdk = '/usr/lib/node_modules/@vue/typescript-plugin'
-                  }
-                }
-              }
-            end
-            require('lspconfig')[server_name].setup(initConfig)
-          end,
-        },
-      })
-    end
-  },
+			require("mason").setup({})
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"tsserver",
+					"volar",
+				},
+				handlers = {
+					function(server_name)
+						local initConfig = {}
+						if server_name == "volar" then
+							initConfig = {
+								filetypes = { "vue" },
+								init_options = {
+									vue = {
+										hybridMode = false,
+									},
+									typescript = {
+										tsdk = "/usr/lib/node_modules/@vue/typescript-plugin",
+									},
+								},
+							}
+						end
+						require("lspconfig")[server_name].setup(initConfig)
+					end,
+				},
+			})
+		end,
+	},
 }

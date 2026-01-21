039219747dd86b0efa1108b968330d12bd90eeb3
Satrio Bagus <satrio.baguswbw@gmail.com>
2026-01-21 10:13:33 +0700

    color temperature toggle script

--- toggle_wlsunset.sh
+++ toggle_wlsunset.sh
@@ -11,9 +11,11 @@ fi
 CURRENT_STATE=$(cat "$STATE_FILE")
 
 if [ "$CURRENT_STATE" -eq 6000 ]; then
-    xsct 4500
+		pkill wlsunset
+    wlsunset -t 4500 &
     echo "4500" > "$STATE_FILE"
 else
-    xsct 6000
+		pkill wlsunset &
+    wlsunset -t 6000
     echo "6000" > "$STATE_FILE"
 fi
